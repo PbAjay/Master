2f4b8d70e99f5f9ef0b2d1ad33ef49ea5e54527c
Matrix TG <108185896+TGxMATRIX@users.noreply.github.com>
2023-05-26 20:57:02 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -13,8 +13,9 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-client = AsyncIOMotorClient(DATABASE_URI) 
-client2 = AsyncIOMotorClient(DATABASE_URI2)  db = client[DATABASE_NAME] 
+client = AsyncIOMotorClient(DATABASE_URI)
+client2 = AsyncIOMotorClient(DATABASE_URI2)
+db = client[DATABASE_NAME]
 db2 = client[DATABASE_NAME2]
 instance = Instance.from_db(db or db2)
 
