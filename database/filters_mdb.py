57a80c7b9a8dd2fe981bda1ebf6c02d37324de69
Rippo123 <98217663+Rippo123@users.noreply.github.com>
2023-05-26 18:57:06 +0530

    addeddoublee db

--- database/filters_mdb.py
+++ database/filters_mdb.py
@@ -6,11 +6,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
 myclient = pymongo.MongoClient(DATABASE_URI)
-myclient2 = pymongo.MongoClient(DATABASE_URI2)
 mydb = myclient[DATABASE_NAME]
-mydb2 = myclient[DATABASE_NAME2]
-
-
 
 async def add_filter(grp_id, text, reply_text, btn, file, alert):
     mycol = mydb[str(grp_id)]
