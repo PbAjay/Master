bfe1f6a68725dc79712f43d02d465f7fe23aa83f
Ajay <106470358+PbAjay@users.noreply.github.com>
2023-05-26 13:42:42 +0530

    💚

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -444,6 +444,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('ᴛᴇʟᴇɢʀᴀᴘʜ', callback_data='tele'),
             InlineKeyboardButton('ꜰᴏɴᴛ', callback_data='font')
         ], [
+            InlineKeyboardButton('ɢɪᴛʜᴜʙ', callback_data='github')
+        ], [
             InlineKeyboardButton('ʙᴀᴄᴋ', callback_data='help2'),
             InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start')
          ]]
@@ -559,6 +561,16 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
+    elif query.data == "github":
+        buttons = [[
+            InlineKeyboardButton('ʙᴀᴄᴋ', callback_data='help')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text=script.GITHUB_TXT,
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        ) 
     elif query.data == "admin":
         buttons = [[
             InlineKeyboardButton('ʙᴀᴄᴋ', callback_data='extra')
