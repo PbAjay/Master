62be3777a61f8cdcebdb3cedb8946fa4efd751b7
AjayZ TG <106470358+PbAjay@users.noreply.github.com>
2023-05-27 10:44:19 +0530

    💚

--- plugins/Mods/ping.py
+++ plugins/Mods/ping.py
@@ -23,7 +23,7 @@ async def ping(_, message):
     rm = await message.reply_text("...")
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
-    await rm.edit(f"<b>ᴩɪɴɢ\n{time_taken_s:.3f} ms</b>")
+    await rm.edit(f"<b>ᴩɪɴɢ\n{time_taken_s:.3f} ᴍꜱ</b>")
 
 
 
