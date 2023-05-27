5d981690be2d13815440760992279e18c17ad77b
Matrix TG <108185896+TGxMATRIX@users.noreply.github.com>
2023-05-27 13:03:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ from database.ia_filterdb import Media
 from database.users_chats_db import db2
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, LOG_CHANNEL
 from utils import temp
-from Script import RESTART_TXT
+from Script import script
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 from datetime import date, datetime 
