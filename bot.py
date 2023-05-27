6f02c028808b9d94801604ca8ae60103305fcdcb
AjayZ TG <106470358+PbAjay@users.noreply.github.com>
2023-05-27 10:25:37 +0530

    💚

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import logging
 import logging.config
 
-# Get logging configurations
+# Get logging configurationss
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
