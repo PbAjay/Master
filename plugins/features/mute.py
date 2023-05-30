f6af7c2333c47e020fe5c05ef2ff7a446e3c4b8a
AjayZ TG <106470358+PbAjay@users.noreply.github.com>
2023-05-30 16:01:11 +0530

    💚

--- plugins/features/mute.py
+++ plugins/features/mute.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import ChatPermissions
-from plugins.helper.admin_check import admin_check
-from plugins.helper.extract import extract_time, extract_user                               
+from plugins.functions.admin_check import admin_check
+from plugins.functions.extract import extract_time, extract_user                               
 
 
 @Client.on_message(filters.command("mute"))
