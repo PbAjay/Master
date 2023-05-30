1e58fb98f08848b0e2fd5bee835489ad1e61ccea
AjayZ TG <106470358+PbAjay@users.noreply.github.com>
2023-05-30 15:02:17 +0530

    💚

--- plugins/GroupHelper /ban.py
+++ plugins/GroupHelper /ban.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
-from plugins.helper.admin_check import admin_check
-from plugins.helper.extract import extract_time, extract_user                               
+from plugins.functions.admin_check import admin_check
+from plugins.functions.extract import extract_time, extract_user                               
 
 
 @Client.on_message(filters.command("ban"))
